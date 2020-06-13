ec540fc57e5794d9f5c947ad5e66ddd4d36736a0
viren4204 <45611825+viren4204@users.noreply.github.com>
2020-06-13 22:09:27 +0530

    modified prime number function

--- prime.js
+++ prime.js
@@ -1,17 +1,17 @@
  function showPrimes(limit){
- 	for(let num = 2; num <= limit; num++) {
-
- 		let isPrime = true;
- 		for (let factor = 2; factor < num; factor++) {
-			if (num % factor === 0) {
-				isPrime = false;
-				break;
-			}
- 		}
- 		if (isPrime) console.log(num);
- 	}
-
+ 	for(let num = 2; num <= limit; num++) 	
+		if (isPrime(num)) console.log(num);
  }
+
+function isPrime(num) {
+	for (let factor = 2; factor < num; factor++) 
+		if (num % factor === 0) 
+			return false;		
+
+	return true;
+}
+
+ showPrimes(100);
  //with break it took 6.39 sec to calculate prime numbers between 1 to 1 lakh.
  // without break  38.90 sec.
 

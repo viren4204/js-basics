4bac8b5e5608b966ff91837443682f10705db88e
viren4204 <45611825+viren4204@users.noreply.github.com>
2020-06-15 20:14:50 +0530

    add template literal

--- string.js
+++ string.js
@@ -5,3 +5,21 @@ const message = 'This is my \nfirst message';
 
 //String object
 const another = new String('hi'); 
+
+//Object {}
+//Boolean true ,false
+//String '', ""
+// Template literal`` (backtik character)
+
+const message_1 = `This is my 
+'third' message. `
+
+let name = 'Biru'
+const emailMessage = 
+`hi ${name}, 
+
+
+Thank you for joining my mailing list
+
+Regards,
+Birendra`;

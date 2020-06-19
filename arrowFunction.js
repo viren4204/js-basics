5590199a8e9d806f80e99a48e181de5fb9733a6f
viren4204 <45611825+viren4204@users.noreply.github.com>
2020-06-19 23:33:15 +0530

    arrow function example

--- arrowFunction.js
+++ arrowFunction.js
@@ -3,8 +3,6 @@ const courses = [
 	{id:2, name: 'b'}	
 ];
 
-const course = courses.find(function(course){
-	return course.name === 'a';
-});
+const course = courses.find(course => course.name === 'a');
 
 console.log(course);

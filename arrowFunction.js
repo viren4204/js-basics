eac40ad0c117560267bb0d2cbf1cb84700e64f72
viren4204 <45611825+viren4204@users.noreply.github.com>
2020-06-19 23:43:58 +0530

    removing element from an array

--- arrowFunction.js
+++ arrowFunction.js
@@ -4,5 +4,21 @@ const courses = [
 ];
 
 const course = courses.find(course => course.name === 'a');
-
 console.log(course);
+
+//==========REMOVING ELEMENTS==========//
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//End
+const last = numbers.pop();
+console.log(last);
+
+//Beginning
+const first = numbers.shift();
+console.log(first);
+
+//Middle
+numbers.splice(4, 2);
+console.log(numbers);
+

5e39108b40313bf22d81a38d608a6b4256685190
viren4204 <45611825+viren4204@users.noreply.github.com>
2020-06-20 00:26:28 +0530

    emting an array

--- arrowFunction.js
+++ arrowFunction.js
@@ -1,3 +1,59 @@
+//========EMPTING AN ARRAY=========//
+let numbers = [1, 2, 3, 4];
+let another = numbers; 
+
+//Solution 1
+// numbers = [];
+
+
+// console.log(another);
+
+//Solution 2
+// numbers.length = 0;
+
+//Solution 3
+// numbers.splice(0, numbers.length);\
+
+//Solution 4
+// while(numbers.length > 0 )
+	// numbers.pop();
+
+console.log(another);
+console.log(numbers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+const obj1 = {a:1, b:2};
+const obj2 = {...obj1};
+
+obj1.b = "sdfsdf";
+
+console.log(obj2);
+
+
+
 const courses = [
 	{id:1, name: 'a'},
 	{id:2, name: 'b'}	
@@ -21,4 +77,5 @@ console.log(first);
 //Middle
 numbers.splice(4, 2);
 console.log(numbers);
+*/
 

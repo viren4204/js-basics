331b860a90f9faa5c5a453e2fa91bfd7c7b7c39e
viren4204 <45611825+viren4204@users.noreply.github.com>
2020-06-20 09:40:42 +0530

    joining arrays, iterating arrays, combining arrays, '

---
+++ concatArray.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//==============JOINING ARRAYS=============//
+const numbers = [1, 2, 3, 4];
+const joined = numbers.join(',');
+console.log(joined);
+
+
+const message = 'This is my first message';
+const parts = message.split(' ');
+console.log(parts);
+
+
+const combined = parts.join('-');
+console.log(combined);
+
+
+
+
+//==========IITERATING ARRAYS============//
+const numbers = [1, 2, 3, 4];
+
+// for (let i of numbers)
+// 	console.log(i);
+
+numbers.forEach(number => console.log(number));
+
+//=======COMBINING ARRAYS========//
+
+
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+const third = [{ id: 1 }]
+
+//Concatanating arrays
+// const combined = first.concat(second);
+// const combined2 = third.concat(second);
+// third[0].id = 45;
+
+const combined = [...first, ...second];
+
+
+// const slice = combine.slice(2,4);
+const copy = [...combined];
+
+
+console.log(combined);
+
+
+
+*/
